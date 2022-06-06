7cc9e62af2777aa1087f33f54a2a62b0d8423d0f
Rhema22 <riemiarh2@gmail.com>
2022-06-06 10:16:49 +0100

    Work on anagrams and words

---
+++ anagram_main.py
@@ -0,0 +1,10 @@
+word1 = 'night'
+ana1 = 'thing'
+word2 = 'devil'
+ana2 = 'lived'
+
+
+if sorted(word1) == sorted(ana1) and sorted(word2) == sorted(ana2) :
+    print('True')
+else:
+    print(False)
